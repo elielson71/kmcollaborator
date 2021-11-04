ddf884050a3c43fcbaa53020d228abf5efa99fb8
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-04 08:56:43 -0300

    api

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 export const api = axios.create({
     baseURL:`${process.env.REACT_APP_API_URL}v1`,
+    headers:{
+        'Content-Type': 'application/json'
+    },
     validateStatus:(status)=> {
         return status < 500;
     },
