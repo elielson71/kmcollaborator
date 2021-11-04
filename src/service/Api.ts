ffa9c3f6fa746af970a8992bc8e05b139c731e16
Eleilson Santos <elielsonboavista@gmail.com>
2021-11-04 13:02:50 -0300

    alteração na conexao com api

--- src/service/Api.ts
+++ src/service/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL:`${process.env.REACT_APP_API_URL}v1`,
+    baseURL:`${process.env.REACT_APP_API_URL}`,
     headers:{
         'Content-Type': 'application/json'
     },
